6a1f11e64a3e1b25a9684c84b91ede21e11895de
FulgurIgor <i.s.ger@ya.ru>
2020-05-14 00:15:26 +0300

    Add info about program and library bits

--- src/main.F90
+++ src/main.F90
@@ -10,6 +10,9 @@ program main
     double precision :: DOT
     A = (/ 1.0, 2.0, 3.0 /)
     B = (/ 1.0, 2.0, 3.0 /)
+    write(6,"(A,I2,A)") "Library: ", LIBRARY, " bit"
+    write(6,"(A,I2,A)") "Program: ", PROGRAM, " bit"
+    flush(6)
     DOT = DDOT(N, A, STEP, B, STEP)
     write(6,"(F6.2)") DOT
 end program main
