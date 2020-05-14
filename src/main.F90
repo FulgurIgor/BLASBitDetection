3bd89e5efd43c32f2f572c594c65742501751ab7
FulgurIgor <i.s.ger@ya.ru>
2020-05-14 17:01:23 +0300

    Add call of IDAMAX subroutine

--- src/main.F90
+++ src/main.F90
@@ -2,12 +2,16 @@ program main
     implicit none
     ! DDOT function from BLAS
     double precision DDOT
+    ! IDAMAX function from BLAS
+    integer(kind=IP) IDAMAX
     ! size of double precision array
     integer(kind=IP), parameter :: N = 3, STEP = 1
     ! array
     double precision :: A(N), B(N)
     ! dot product
     double precision :: DOT
+    ! idamax value
+    integer(kind=IP) :: DAMAX
     A = (/ 1.0, 2.0, 3.0 /)
     B = (/ 1.0, 2.0, 3.0 /)
     write(6,"(A,I2,A)") "Library: ", LIBRARY, " bit"
@@ -15,4 +19,6 @@ program main
     flush(6)
     DOT = DDOT(N, A, STEP, B, STEP)
     write(6,"(F6.2)") DOT
+    DAMAX = IDAMAX(N,A,STEP)
+    write(6,"(I3)") DAMAX
 end program main
